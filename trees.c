2c28372df025991f2a7c1af618d14296c5362221
aks <aksjjw@gmail.com>
2024-03-09 11:55:18 -0500

    converted tree to binary search tree

--- trees.c
+++ trees.c
@@ -1,5 +1,10 @@
+//Binary Search Tree is only fast if the tree is fairly balanced
+// time complexity for searching an element is O(log(n))
+// ways to implement BST that take care of situations where the tree would otherwise not end up being well balanced such as if the data is already sorted, it would mostly look like a normal linked list not a tree: AVL, red & black tree
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct treenode {
     int value;
@@ -50,25 +55,58 @@ void printtree(treenode* root) {
     printtree_rec(root, 0);
 }
 
+bool insertnumber(treenode **rootptr, int value) {
+    treenode *root = *rootptr;
+    if (root == NULL) { // first base case
+        //tree empty
+        (*rootptr) = createnode(value);
+        return true;
+    }
+    if (value == root->value) { //second base base
+        //do nothing
+        return false;
+    }
+
+    //now the 2 recursive cases we have
+    if (value < root->value) {
+        return insertnumber(&(root->left), value);
+    } else {
+        return insertnumber(&(root->right), value);
+    }
+}
+
+bool findnumber(treenode * root, int value) { // no double ptr needed this time because we're not changing the tree in any way in this function, so single ptr should suffice
+    if (root == NULL) return false; //1st base case
+    if (root->value == value) { //2nd base case
+        return true;
+    }
+    
+    //recursive cases
+    if (value < root->value) {
+        return findnumber(root->left, value);
+    } else {
+        return findnumber(root->right, value);
+    }
+}
+
 int main() {
     
-    treenode *n1 = createnode(10);
-    treenode *n2 = createnode(11);
-    treenode *n3 = createnode(12);
-    treenode *n4 = createnode(13);
-    treenode *n5 = createnode(14);
+    treenode *root = NULL;
 
-    n1->left = n2;
-    n1->right = n3;
-    n3->left = n4;
-    n3->right = n5;
+    insertnumber(&root, 15);
+    insertnumber(&root, 11);
+    insertnumber(&root, 24);
+    insertnumber(&root, 5);
+    insertnumber(&root, 19);
+    insertnumber(&root, 16);
 
-    printtree(n1); 
+    printtree(root); 
 
-    free(n1);
-    free(n2);
-    free(n3);
-    free(n4);
-    free(n5);
+    printf("%d (%d)\n", 16, findnumber(root, 16));
+    printf("%d (%d)\n", 15, findnumber(root, 15));
+    printf("%d (%d)\n", 5, findnumber(root, 5));
+    printf("%d (%d)\n", 115, findnumber(root, 115));
+    printf("%d (%d)\n", 1, findnumber(root, 1));
+    printf("%d (%d)\n", 7, findnumber(root, 7));
 
 }
