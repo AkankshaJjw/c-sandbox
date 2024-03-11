90ff8370bda73831b9e1f391c7099b626bed7880
aks <aksjjw@gmail.com>
2024-03-10 22:52:06 -0400

    added examples to show qsort and bsearch

--- std_libs_test.c
+++ std_libs_test.c
@@ -6,8 +6,6 @@
 #include <stdarg.h>
 #include <time.h>
 
-// based on lesson 10 of MIT practical programming with C
-
 /* ctype.h example - Testing characters */
 void test_characters() {
     char c;
@@ -79,6 +77,51 @@ void time_examples() {
     printf("Current local time and date: %s", asctime(timeinfo));
 }
 
+/* Comparison function for qsort */
+int compare_ints(const void* a, const void* b) {
+    int arg1 = *(const int*)a;
+    int arg2 = *(const int*)b;
+    if (arg1 < arg2) return -1;
+    if (arg1 > arg2) return 1;
+    return 0;
+}
+
+/* Example to show to search an integer array */
+void search_integer_array() {
+    int values[] = { 40, 10, 100, 90, 20, 25 };
+    int size = sizeof(values) / sizeof(int);
+    int key = 25;
+    int *item;
+
+    /* First sort the array */
+    qsort(values, size, sizeof(int), compare_ints);
+
+    /* Then use bsearch to find the value */
+    item = (int*) bsearch(&key, values, size, sizeof(int), compare_ints);
+
+    if (item) {
+        printf("Found item: %d\n", *item);
+    } else {
+        printf("Item %d not found\n", key);
+    }
+}
+
+/* Example to show to sort an integer array */
+void sort_integer_array() {
+    int values[] = { 40, 10, 100, 90, 20, 25 };
+    int size = sizeof(values) / sizeof(int);
+
+    /* Sort the array */
+    qsort(values, size, sizeof(int), compare_ints);
+
+    /* Print the sorted array */
+    printf("Sorted array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", values[i]);
+    }
+    printf("\n");
+}
+
 int main() {
     /* ctype.h example function call */
     test_characters();
@@ -94,6 +137,9 @@ int main() {
 
     /* time.h example function call */
     time_examples();
+
+    search_integer_array();
+    sort_integer_array();
 
     return 0;
 }
